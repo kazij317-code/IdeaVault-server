0214f0bdb3962040587c8f253c0edfe8df98ff7f
kazij317-code <kazij317@gmail.com>
2026-05-19 13:10:51 +0600

    Created API for get All idea's data and Single idea's data

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 const port = process.env.PORT || 5000;
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = process.env.MONGODB_URI;
+const uri = "mongodb+srv://ideavault:z5jiJq9cRlotHIEW@cluster0.wenolut.mongodb.net/?appName=Cluster0";
 
 
 const client = new MongoClient(uri, {
@@ -22,9 +22,36 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 });
+    // await client.db('admin').command({ ping: 1 });
+
+const db = client.db('ideavault');
+const ideasCollection = db.collection('ideas');
+
+app.get('/ideas', async (req, res) => {     
+      let cursor = ideasCollection.find();
+      const result = await cursor.toArray();
+      // console.log(result);
+      res.send(result);
+    })
+
+
+   app.get('/ideas/:ideaId', async (req, res) => {
+      // const ideaId = req.params.ideaId;
+      //   console.log(req.user, 'req');
+
+      const { ideaId } = req.params;
+      //   console.log(ideaId);
+      const query = { _id: new ObjectId(ideaId) };
+      const result = await ideasCollection.findOne(query);
+      res.send(result);
+    });
+
+
+
+
+
  console.log('Pinged your deployment. You successfully connected to MongoDB!');
   } finally {
     // Ensures that the client will close when you finish/error

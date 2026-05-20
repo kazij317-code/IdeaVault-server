6dbfdf50badc7eac7246f857a605c616befda1e0
kazij317-code <kazij317@gmail.com>
2026-05-20 09:37:35 +0600

    some edit has completed

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
 dotenv.config();
 const app = express();
 app.use(cors());
-
+app.use(express.json());
 const port = process.env.PORT || 5000;
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = MONGODB_URI;
+const uri = process.env.MONGODB_URI;
 
+const JWKS = createRemoteJWKSet(new URL(`${process.env.CLIENT_URL}/api/auth/jwks`));
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -35,7 +38,7 @@ const verifyToken = async (req, res, next) => {
   }
 
   try {
-    const JWKS = createRemoteJWKSet(new URL('${process.env.CLIENT_URL}/api/auth/jwks'));
+    const JWKS = createRemoteJWKSet(new URL('http://localhost:3000/api/auth/jwks'));
     const { payload } = await jwtVerify(token, JWKS);
     req.user = payload;
 
@@ -46,21 +49,17 @@ const verifyToken = async (req, res, next) => {
   }
 };
 
-
-
-
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db('admin').command({ ping: 1 });
+    const db = client.db('ideavault');
+    const ideasCollection = db.collection('ideas');
+    // const enrollmentCollection = db.collection('enrollments');
 
-const db = client.db('ideavault');
-const ideasCollection = db.collection('ideas');
-
-// Create API for all ideas
-app.get('/ideas', async (req, res) => {     
+    app.get('/ideas', async (req, res) => {
       const { search, category } = req.query;
 
       let query = {};
@@ -141,23 +140,35 @@ app.get('/ideas', async (req, res) => {
           ]
         };
       }
-  
-  let cursor = ideasCollection.find();
-      const result = await cursor.toArray();
-      // console.log(result);
-      res.send(result);
-    })
 
-// Create API for featured data
-app.get('/featured', async (req, res) => {
+      let cursor = ideasCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get('/featured', async (req, res) => {
       const cursor = ideasCollection.find().limit(6);
       const result = await cursor.toArray();
       res.send(result);
     });
 
+    app.post('/ideas', verifyToken, async (req, res) => {
+      try {
+        const ideaData = req.body;
+        const newidea = {
+          ...ideaData,
+          enrollCount: 0,
+          createdAt: new Date(),
+        };
+        const result = await ideasCollection.insertOne(newidea);
+        res.status(201).send(result);
+      } catch (err) {
+        console.error('Error adding idea:', err);
+        res.status(500).json({ message: 'Internal server error' });
+      }
+    });
 
-// Create API for single idea
-app.get('/ideas/:ideaId', async (req, res) => {
+    app.get('/ideas/:ideaId', logger, verifyToken, async (req, res) => {
       // const ideaId = req.params.ideaId;
       //   console.log(req.user, 'req');
 
@@ -168,11 +179,43 @@ app.get('/ideas/:ideaId', async (req, res) => {
       res.send(result);
     });
 
+    // app.get('/enrollments/:userId', verifyToken, async (req, res) => {
+    //   const { userId } = req.params;
+    //   const result = await enrollmentCollection.find({ userId: userId }).toArray();
+    //   res.send(result);
+    // });
 
+    // app.patch('/enrollments/:ideaId', verifyToken, async (req, res) => {
+    //   //   console.log('from enrollment');
 
+    //   const { ideaId } = req.params;
+    //   const enrollmentData = req.body;
 
+    //   const idea = await ideasCollection.findOne({ _id: new ObjectId(ideaId) });
 
- console.log('Pinged your deployment. You successfully connected to MongoDB!');
+    //   if (!idea) {
+    //     return res.status(404).json({ message: 'idea not found' });
+    //   }
+    //   await ideasCollection.updateOne(
+    //     { _id: new ObjectId(ideaId) },
+    //     {
+    //       $inc: { enrollCount: 1 },
+    //       $set: {
+    //         lastEnrolledAt: new Date(),
+    //       },
+    //     }
+    //   );
+    //   //   console.log(enrollmentData);
+
+    //   const result = await enrollmentCollection.insertOne({
+    //     ...enrollmentData,
+    //     enrolledAt: new Date(),
+    //   });
+
+    //   res.send(result);
+    // });
+
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -180,13 +223,10 @@ app.get('/ideas/:ideaId', async (req, res) => {
 }
 run().catch(console.dir);
 
-
-
-
 app.get('/', (req, res) => {
-  res.send('Hello Assignment9!');
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`App is running well on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });

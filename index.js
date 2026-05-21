6b35a58fb06f297b1d99a4f0e78afa7497d5fd01
kazij317-code <kazij317@gmail.com>
2026-05-21 14:57:29 +0600

    problem solved

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const verifyToken = async (req, res, next) => {
   }
 
   try {
-    const JWKS = createRemoteJWKSet(new URL('http://localhost:3000/api/auth/jwks'));
+    const JWKS = createRemoteJWKSet(new URL(`${process.env.CLIENT_URL}/api/auth/jwks`));
     const { payload } = await jwtVerify(token, JWKS);
     req.user = payload;
 
